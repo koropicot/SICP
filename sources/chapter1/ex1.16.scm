c46a350e5313b228b395bcac1745bcc0d8f294e1
koropicot <koropicot@gmail.com>
2014-08-08 16:19:07 +0900

    問題1.16を追加

---
+++ sources/chapter1/ex1.16.scm
@@ -0,0 +1,11 @@
+;対数オーダーの反復的プロセスの冪乗手続き
+
+(define (square x) (* x x))
+(define (even? x) (= (remainder x 2) 0))
+
+(define (fast-expt-ip b n)
+  (define (iter a b n)
+    (cond ((= n 0) a)
+          ((even? n) (iter a (square b) (/ n 2)))
+          (else (iter (* a b) b (- n 1)))))
+  (iter 1 b n))
